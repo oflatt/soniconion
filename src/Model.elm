a478c1839e1fef51e246b0bff8f892ecfdf856a2
oliver <oflatt@gmail.com>
2019-10-05 14:17:46 -0600

    added basic type for Onion program

--- src/Model.elm
+++ src/Model.elm
@@ -4,7 +4,12 @@ import Url
 import Browser
 import Browser.Navigation as Nav
 
-type Msg = MouseOver PageName | MouseLeave PageName | LinkClicked Browser.UrlRequest | PageChange String | UrlChanged Url.Url | WindowResize Int Int
+type Msg = MouseOver PageName
+         | MouseLeave PageName
+         | LinkClicked Browser.UrlRequest
+         | PageChange String
+         | UrlChanged Url.Url
+         | WindowResize Int Int
 
 urlToPageName url =
     if url.path == "/" then
@@ -16,13 +21,26 @@ urlToPageName url =
 type alias PageName = String
 type alias MousePos = (Float, Float)
 
+type alias Id = Int
+type alias Constant = Int
+-- id of function output or a constant
+type Input = Output Id
+           | Const Constant
+
+type alias Onion = List Function
+type alias Function = List Expr
+type alias Expr = {id: Id
+                  ,inputs: List Input
+                  ,funcName: String}
+    
 type alias Model = {currentPage: PageName
                    ,highlightedButton: PageName
                    ,urlkey : Nav.Key
                    ,url : Url.Url
                    ,indexurl : String
                    ,windowWidth : Int
-                   ,windowHeight : Int}
+                   ,windowHeight : Int
+                   ,program : Onion}
 
 getindexurl url =
     let str = (Url.toString url)
@@ -36,7 +54,11 @@ type alias Flags = {innerWindowWidth : Int,
                    outerWindowHeight : Int}
 
 
-    
+initialProgram = [[(Expr 1 [Const 1, Const 440] "sine")
+                  ,(Expr 2 [Output 1] "play")
+                  ]
+                 ] 
+ 
 initialModel : Flags -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
 initialModel flags url key = ((Model
                                    (urlToPageName url)
@@ -44,4 +66,6 @@ initialModel flags url key = ((Model
                                    key url
                                    (getindexurl url)
                                    flags.innerWindowWidth
-                                   flags.innerWindowHeight), Cmd.none)
+                                   flags.innerWindowHeight
+                                   initialProgram),
+                                  Cmd.none)
