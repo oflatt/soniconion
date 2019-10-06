4b3fa16b2fa8c58096df217d59e8651b67e25c13
oliver <oflatt@gmail.com>
2019-10-06 07:00:26 -0600

    refactor SvgAssets

--- src/DrawProgram.elm
+++ src/DrawProgram.elm
@@ -1,157 +1,33 @@
 module DrawProgram exposing (drawProgram)
 
-import Browser
-
 import Model exposing (..)
+import SvgAssets
+
+
+import Browser
 
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css, href, src, rel)
 import Html.Styled.Events exposing (onClick, onMouseOver, onMouseLeave)
-import Html.Attributes exposing (id)
-import Svg
-import Svg.Attributes
+
 
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
-import Browser
-import Browser
+
 
 import Html.Events exposing (onInput)
 
-paddingSize = 20
 
-mainShape =
- {- svg
-    [ viewBox "0 0 400 400"
-    , width "400"
-    , height "400"
-    ]-}
-    [rect
-        [ x "30"
-        , y "50"
-        , width "200"
-        , height "80"
-        , fill "green"
-        , stroke "green"
-        , strokeWidth "2"
-        ]
-        []
-     , rect
-       [ x "30"
-        , y "30"
-        , width "80"
-        , height "30"
-        , fill "green"
-        , stroke "green"
-        , strokeWidth "2"
-        ]
-        []
-     , text_
-        [ x "70"
-        , y "40"
-        , fill "white"
-        , textAnchor "middle"
-        , dominantBaseline "central"
-        ]
-        [ Svg.text "Main"
-        ]
-        ]
-
--- shape for functionName objects
-functionNameshape: Int -> (Svg msg)
-functionNameshape yPos =
-  Svg.node "g"
-      [
-       transform ("translate(" ++ "30," ++(String.fromInt (paddingSize + yPos) ++ ")"))
-      ]
-      {-svg
-      [ viewBox "0 0 400 400"
-      , width "400"
-      , height "400"
-      ]-}
-      [ circle
-            [ cx "20"
-            , cy "20"
-            , r "20"
-            , fill "red"
-            , stroke "red"
-            , strokeWidth "3"
-            ]
-            []
-      , rect
-            [ x "0"
-            , y "20"
-            , width "200"
-            , height "80"
-            , fill "red"
-            , stroke "red"
-            , strokeWidth "2"
-            ]
-            []
-      ]
-      
-methodNameShape =
-    [ circle
-          [ cx "50"
-          , cy "165"
-          , r "20"
-          , fill "orange"
-          , stroke "orange"
-          , strokeWidth "3"
-          ]
-          []
-    , rect
-          [ x "30"
-          , y "165"
-          , width "200"
-          , height "80"
-          , fill "orange"
-          , stroke "orange"
-          , strokeWidth "2"
-        ]
-      []
-    ]
-    
-lineVertical=
-  [line
-    [x1  "260"
-    , y1  "400"
-    , x2  "260"
-    , y2  "460"
-    , stroke  "blue"
-    , strokeWidth  "5"
-    , strokeLinecap  "round"
-    ]
-  []
-  ]
-lineHorizontal =
-  [line
-    [x1  "200"
-    , y1  "400"
-    , x2  "240"
-    , y2  "400"
-    , stroke  "purple"
-    , strokeWidth  "5"
-    , strokeLinecap  "round"
-    ]
-    []
-    ]
-createViewboxDimensions modelWidth modelHeight =
-    let
-        width = String.fromInt (600)
-        height = String.fromInt (600)
-    in
-        width ++ " " ++ height
-
+              
 -- function for drawing builtIns
 drawBuiltIn: BuiltIn -> Int -> (Svg msg)
 drawBuiltIn builtIn counter =
-  functionNameshape (counter * 200)
+    SvgAssets.functionNameshape (counter * SvgAssets.blockSpacing)
 
 -- function for drawing play
 drawPlay: Play -> Int -> (Svg msg)
 drawPlay play counter =
-  functionNameshape (counter * 200)
+  SvgAssets.functionNameshape (counter * 200)
 
 -- function for drawing Expression objects
 drawExpression: Expr -> Int -> (Svg msg)
@@ -186,7 +62,7 @@ drawProgram model width height =
     (Svg.svg
         [ Svg.Attributes.width(String.fromInt width) -- define the width of the svg
         , Svg.Attributes.height(String.fromInt height) -- define the height of the svg
-        , Svg.Attributes.viewBox("0 0 " ++ createViewboxDimensions width height) -- define the viewbox
+        , Svg.Attributes.viewBox("0 0 " ++ SvgAssets.createViewboxDimensions width height) -- define the viewbox
         , display "inline-block"
         ]
          -- (mainShape ++ functionNameshape ++ methodNameShape ++ lineVertical ++ lineHorizontal))
---
+++ src/SvgAssets.elm
@@ -0,0 +1,138 @@
+module SvgAssets exposing (..)
+
+
+import Model exposing (..)
+
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css, href, src, rel)
+import Html.Styled.Events exposing (onClick, onMouseOver, onMouseLeave)
+
+
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+blockSpacing = 150
+paddingSize = 20
+
+mainShape =
+ {- svg
+    [ viewBox "0 0 400 400"
+    , width "400"
+    , height "400"
+    ]-}
+    [rect
+        [ x "30"
+        , y "50"
+        , width "200"
+        , height "80"
+        , fill "green"
+        , stroke "green"
+        , strokeWidth "2"
+        ]
+        []
+     , rect
+       [ x "30"
+        , y "30"
+        , width "80"
+        , height "30"
+        , fill "green"
+        , stroke "green"
+        , strokeWidth "2"
+        ]
+        []
+     , text_
+        [ x "70"
+        , y "40"
+        , fill "white"
+        , textAnchor "middle"
+        , dominantBaseline "central"
+        ]
+        [ Svg.text "Main"
+        ]
+        ]
+
+-- shape for functionName objects
+functionNameshape: Int -> (Svg msg)
+functionNameshape yPos =
+  Svg.node "g"
+      [
+       transform ("translate(" ++ "30," ++(String.fromInt (paddingSize + yPos) ++ ")"))
+      ]
+      {-svg
+      [ viewBox "0 0 400 400"
+      , width "400"
+      , height "400"
+      ]-}
+      [ circle
+            [ cx "20"
+            , cy "20"
+            , r "20"
+            , fill "red"
+            , stroke "red"
+            , strokeWidth "3"
+            ]
+            []
+      , rect
+            [ x "0"
+            , y "20"
+            , width "200"
+            , height "80"
+            , fill "red"
+            , stroke "red"
+            , strokeWidth "2"
+            ]
+            []
+      ]
+      
+methodNameShape =
+    [ circle
+          [ cx "50"
+          , cy "165"
+          , r "20"
+          , fill "orange"
+          , stroke "orange"
+          , strokeWidth "3"
+          ]
+          []
+    , rect
+          [ x "30"
+          , y "165"
+          , width "200"
+          , height "80"
+          , fill "orange"
+          , stroke "orange"
+          , strokeWidth "2"
+        ]
+      []
+    ]
+    
+lineVertical=
+  [line
+    [x1  "260"
+    , y1  "400"
+    , x2  "260"
+    , y2  "460"
+    , stroke  "blue"
+    , strokeWidth  "5"
+    , strokeLinecap  "round"
+    ]
+  []
+  ]
+lineHorizontal =
+  [line
+    [x1  "200"
+    , y1  "400"
+    , x2  "240"
+    , y2  "400"
+    , stroke  "purple"
+    , strokeWidth  "5"
+    , strokeLinecap  "round"
+    ]
+    []
+    ]
+createViewboxDimensions modelWidth modelHeight =
+    let
+        width = String.fromInt (600)
+        height = String.fromInt (600)
+    in
+        width ++ " " ++ height
