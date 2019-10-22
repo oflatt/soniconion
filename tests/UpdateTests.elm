a6e8393b650117cd6e019c7c9336678c0f6a3292
oliver <oflatt@gmail.com>
2019-10-22 13:18:05 -0600

    add travis build

--- tests/UpdateTests.elm
+++ tests/UpdateTests.elm
@@ -10,13 +10,14 @@ sine = (Call 80 [Const 1, Const 440] "sine")
 sine2 = (Call 89 [Const 2, Const 640] "sine")
 join = (Call 85 [Output 80, Output 89] "join")
 play = (Call 1092392 [Output 85] "play")
+playWithHole = (Call 1092392 [Hole] "play")
        
 -- play is assumed to be at the end
 testFunction : Function
-testFunction = [sine
-               ,sine2
-               ,join
-               ,play]
+testFunction = [sine, sine2, join, play]
+testInvalidFunction : Function
+testInvalidFunction = [sine, sine2, play, join]
+    
                
 
 fixInvalidInputs : Test
@@ -25,7 +26,11 @@ fixInvalidInputs =
         [test "unchanged example"
              (\_ -> (Expect.equal
                          (Update.fixInvalidInputs testFunction)
-                         testFunction))]
+                         testFunction))
+        ,test "fix play"
+            (\_ ->
+                 (Expect.equal (Update.fixInvalidInputs testInvalidFunction)
+                      [sine, sine2, playWithHole, join]))]
                 
 
 
